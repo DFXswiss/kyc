d1924ddb9e687d00b53795266d2dc0d6c7e1c6bb
Kolibri <unknown>
2023-07-13 15:21:40 +0200

    [DEV-1591]  Add kycSteps to user info

--- src/subdomains/user/api/dto/user-info.mapper.ts
+++ src/subdomains/user/api/dto/user-info.mapper.ts
@@ -1,11 +1,13 @@
 import { User } from 'src/subdomains/user/entities/user.entity';
-import { UserInfoDto } from '../dto/user-out.dto';
+import { KycStepMapper } from './kyc-step.mapper';
+import { UserInfoDto } from './user-out.dto';
 
 export class UserInfoMapper {
   static toDto(user: User): UserInfoDto {
     const dto: UserInfoDto = {
       language: user.language,
       kycStatus: user.kycStatus,
+      kycSteps: KycStepMapper.entitiesToDto(user.kycSteps),
     };
 
     return Object.assign(new UserInfoDto(), dto);
--- src/subdomains/user/api/dto/user-out.dto.ts
+++ src/subdomains/user/api/dto/user-out.dto.ts
@@ -1,6 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { LanguageDto } from 'src/subdomains/master-data/language/dto/language.dto';
 import { KycStatus } from 'src/subdomains/user/entities/user.entity';
+import { KycStepName, KycStepStatus } from '../../entities/kyc-step.entity';
+
+export class KycStepDto {
+  @ApiProperty({ type: KycStepName })
+  name: KycStepName;
+
+  @ApiProperty({ type: KycStepStatus })
+  status: KycStepStatus;
+
+  @ApiProperty()
+  sessionUrl: string;
+}
 
 export class UserInfoDto {
   @ApiProperty({ type: LanguageDto })
@@ -9,5 +21,6 @@ export class UserInfoDto {
   @ApiProperty({ enum: KycStatus })
   kycStatus: KycStatus;
 
-  // TODO
+  @ApiProperty({ type: KycStepDto, isArray: true })
+  kycSteps: KycStepDto[];
 }
